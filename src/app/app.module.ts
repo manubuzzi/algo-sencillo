43e52615a2291ed43012432ec40895280e5a3398
manubuzzi <manubuzzi13@gmail.com>
2021-06-17 01:23:19 -0300

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
+import { NewTasksComponent } from './components/new-tasks/new-tasks.component';
+import { ListTasksComponent } from './components/list-tasks/list-tasks.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTaskComponent,
+    NewTasksComponent,
+    ListTasksComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
